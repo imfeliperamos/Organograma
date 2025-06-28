43301dcc9af1e44168170d172108fed7397df7d2
Valdir Felipe Dos Santos Ramos <5297607@alunouninter.com>
2025-06-28 15:53:36 -0300

    Estudando react
    Estudando react

---
+++ src/componentes/Banner/banner.js
@@ -0,0 +1,14 @@
+import './banner.css'
+
+function Banner() {
+    // JSX PARECE HTML MAS NÃO É ELE É JSX
+    // Return de multiplas linhas
+    return (
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="Banner"></img>
+        </header>
+    )
+}
+
+export default Banner
+// Precisamos exportar o function
---
+++ src/componentes/formulario/index.js
@@ -0,0 +1,19 @@
+import './Formulario.css'
+
+const Formulario = () => {
+    return (
+
+        <section>
+            <form>
+                <CampoTexto label='Nome' placeholder='Digite seu nome' />
+                <CampoTexto label='Cargo' placeholder='Digite seu cargo' />
+                <CampoTexto label='Imagem' placeholder='Informe o endereço da imagem' />
+
+            </form>
+        </section>
+
+    )
+
+}
+
+export default Formulario
